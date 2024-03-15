adf9126aa38252f3d1cc660e6874b68026a9a5c3
Pierre-Louis Létoquart <pierrelouislet@gmail.com>
2024-03-15 17:28:04 -0400

    cleanup code

--- index.ts
+++ index.ts
@@ -3,16 +3,28 @@ import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { StringOutputParser } from "@langchain/core/output_parsers";
 import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import type { Document } from "langchain/document";
 import { OllamaEmbeddings } from "langchain/embeddings/ollama";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { createRetrievalChain } from "langchain/chains/retrieval";
-import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import type { Document } from "langchain/document";
+
+const stdin = process.stdin;
+const stdout = process.stdout;
 
 const prompt = ChatPromptTemplate.fromMessages([
   ["system", "You are a helpful assistant."],
   ["user", "{input}"],
 ]);
+
+const prompt_retrieval =
+  ChatPromptTemplate.fromTemplate(`Answer the following question based only on the provided context:
+
+<context>
+{context}
+</context>
+
+Question: {input}`);
 
 const embeddings = new OllamaEmbeddings({
   model: "nomic-embed-text",
@@ -28,16 +40,25 @@ const outputParser = new StringOutputParser();
 
 const llmChain = prompt.pipe(chatModel).pipe(outputParser);
 
+const documentChain = await createStuffDocumentsChain({
+  llm: chatModel,
+  prompt: prompt_retrieval,
+});
+
 async function getInput(question: string): Promise<string> {
   return new Promise((resolve) => {
-    const stdin = process.stdin;
-    const stdout = process.stdout;
     stdin.resume();
     stdout.write(question);
     stdin.once("data", (data) => {
       resolve(data.toString().trim());
     });
   });
+}
+
+function modelOutput(res: string) {
+  stdout.write("(assistant) ==> ");
+  stdout.write(res);
+  stdout.write("\n\n");
 }
 
 async function processInput(input: string): Promise<Document[] | null> {
@@ -47,54 +68,39 @@ async function processInput(input: string): Promise<Document[] | null> {
     return null;
   }
 
+  stdout.write("(system) ==> Loading context...");
   const loader = new CheerioWebBaseLoader(urls.join(","));
   const docs = await loader.load();
 
   const splitter = new RecursiveCharacterTextSplitter();
   const splitDocs = await splitter.splitDocuments(docs);
 
+  stdout.write("Done\n");
   return splitDocs;
 }
 
-while (true) {
-  console.log(
-    "Enter a question to ask the model, if you want to use a context, provide a link to a document.",
-  );
-  const input = await getInput("==> ");
+console.log(
+  "Enter a question to ask the model, if you want to use a context, provide a link to a document.",
+);
 
+while (true) {
+  const input = await getInput("==> ");
   const docs = await processInput(input);
 
   if (docs) {
-    console.log("Loading context...");
+    stdout.write("(assistant) ==> Thinking...\n");
     const vectorstore = await MemoryVectorStore.fromDocuments(docs, embeddings);
-
-    const prompt =
-      ChatPromptTemplate.fromTemplate(`Answer the following question based only on the provided context:
-
-    <context>
-    {context}
-    </context>
-
-    Question: {input}`);
-
-    const documentChain = await createStuffDocumentsChain({
-      llm: chatModel,
-      prompt,
-    });
-
-    // We will use the vectorstore as a retriever
     const retriever = vectorstore.asRetriever();
-
     const retrievalChain = await createRetrievalChain({
       combineDocsChain: documentChain,
       retriever,
     });
 
     const res = await retrievalChain.invoke({ input });
-
-    console.log(res);
+    modelOutput(res.answer);
   } else {
+    stdout.write("(assistant) ==> Thinking...\n");
     const res = await llmChain.invoke({ input });
-    console.log(res);
+    modelOutput(res);
   }
 }
